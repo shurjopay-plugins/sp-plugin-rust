bbb8a623d7d8470bc8ba1535cf3db165d7d97c08
shakil.anwar <root@DESKTOP-A426QTV.localdomain>
2022-11-08 13:49:18 +0600

    added comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,23 @@
+// #![deny(missing_docs)]
+// #![deny(missing_debug_implementations)]
+// #![cfg_attr(docsrs, feature(doc_cfg))]
+// #![cfg_attr(test, deny(warnings))]
 
-mod Shurjopay;
+//! # Shurjopay
+//!
+//! The `Shurjopay` crate provides a convenient way to integrate 
+//! [`Shurjopay`][shurjopay] payment gateway.
+//!
+//! It handles many of the things that most people need to do manually
+//! 
+//! - Handles http request and request errors
+//! - JSON serialization and deserialization
+//! - Authentication during checkout and verification of payments
+//!
+//! 
+//! [shurjopay]: crate::Shurjopay
+
+pub mod Shurjopay;
 use Shurjopay::ShurjopayPlugin;
 
 

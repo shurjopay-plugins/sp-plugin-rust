48e6fcaca352629f9ca2ab5539ed17e964c41b1d
mahmudulislam299 <mahmudulislam299@gmail.com>
2022-12-31 18:30:27 +0600

    test file modified

--- tests/test.rs
+++ tests/test.rs
@@ -1,7 +1,7 @@
 #[cfg(test)]
 mod tests {
 
-    use sp_plugin_rust_test::shurjopay::ShurjopayPlugin;
+    use sp_plugin_rust::shurjopay::ShurjopayPlugin;
     use assert_str::assert_str_eq;
     use webbrowser;
 
